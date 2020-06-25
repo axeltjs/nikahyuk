29bc3fcf2b6694834206292f4a9ed8092b45a4f1
axeltjs <axel.tjsaputra@vmlyr.com>
2020-06-25 21:49:02 +0800

    bug fixed

--- app/Http/Controllers/Admin/ChatController.php
+++ app/Http/Controllers/Admin/ChatController.php
@@ -92,10 +92,9 @@ class ChatController extends Controller
             if ($chat !== null) {
                 $data = $chat->chatMessage;
 
-                $transaction = $this->transaction->where(['vendor_id' => $chat->vendor_id, 'customer_id' => $chat->customer_id])
-                    ->where('status', 1)
+                $transaction = $this->transaction->where('status', '>=' ,1)->where(['vendor_id' => $chat->vendor_id, 'customer_id' => $chat->customer_id])
                     ->count();
-
+                    
                 $quotations = $this->quotation->where(['creator_id' => $chat->vendor_id, 'customer_id' => $chat->customer_id])
                     ->pluck('package_name', 'id');
             }
